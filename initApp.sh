752bc8fa7196b345dd8426f4508b370d769116ad
TannerB <tannerpace@gmail.com>
2023-03-23 18:57:28 -0400

    fix

--- initApp.sh
+++ initApp.sh
@@ -12,15 +12,45 @@ curl -u $github_username https://api.github.com/user/repos -d "{\"name\":\"$repo
 echo "Enter the name of your new TypeScript app:"
 read app_name
 
-turbo new $app_name --template typescript
 
-cd $app_name
+
+# Create a new local repository
+git init
+
+# Create a new README.md file
+touch README.md
+
+# Create a new .gitignore file
+touch .gitignore
+
+# Add the .gitignore file to the local repository
+git add .gitignore
+
+# Commit the .gitignore file to the local repository
+git commit -m "Add .gitignore"
+
+# Add the README.md file to the local repository
+
+git add README.md
+
+# Commit the README.md file to the local repository
+git commit -m "Add README.md"
+
+# Create a new TypeScript app with Turbo Repo
+
+yarn dlx create-turbo@latest $app_name
 
 # Install required packages
 yarn add react-query zod @mui/material @emotion/react @emotion/styled axios zod @mui/icons-material @mui/lab @mui/x-data-grid @mui/x-data-grid-pro @mui/x-data-grid-generator 
 
 # Add a new remote to the local repository
 git remote add origin 
+
+# Add the new TypeScript app to the local repository
+git add .
+
+# Commit the new TypeScript app to the local repository
+git commit -m "Add $app_name"
 
 # Push the local repository to the remote
 git push -u origin main
